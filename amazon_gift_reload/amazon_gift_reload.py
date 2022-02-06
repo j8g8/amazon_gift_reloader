8ee4e92568b22bc4cea3eb8ae2013df5c50f26f5
gilbertoid <38501559+gilbertoid@users.noreply.github.com>
2022-02-05 20:12:08 -0500

    rm bad assert

--- amazon_gift_reload/amazon_gift_reload.py
+++ amazon_gift_reload/amazon_gift_reload.py
@@ -1,5 +1,4 @@
 """Main module."""
-# https://pypi.org/project/chromedriver-py/
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -11,12 +10,13 @@ import logging
 class AmazonGiftReloader(object):
     RELOAD_URL = "https://www.amazon.com/gp/product/B086KKT3RX"
     SIGNIN_URL = "https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fproduct%2FB086KKT3RX%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&"
-    PLACE_ORDER_XPATH = "/html/body/div[5]/div/div[2]/form/div/div/div/div[2]/div/div[1]/div/div[1]/div/span/span/input"
+    PLACE_ORDER_BUTTON_NAME = "placeYourOrder1"
 
     PAGE_LOAD_WAIT_SECS = 10
 
     def __init__(self, reload_value, number_of_reloads, email, password) -> None:
         self.logger = logging.getLogger("amazon_gift_reloader.AmazonGiftReloader")
+        # logging.basicConfig(level=logging.INFO)
 
         self.reload_value = reload_value
         self.number_of_reloads = number_of_reloads
@@ -73,12 +73,11 @@ class AmazonGiftReloader(object):
     def input_reload_value(self):
         amount_field = self.find_elem_by_id("gcui-asv-reload-form-custom-amount")
         self.input_value(amount_field, self.reload_value)
-        assert self.find_elem_by_id("placeYourOrder")
         return
 
     def submit_order(self):
         self.logger.info("submitting the order!")
         WebDriverWait(self.driver, self.PAGE_LOAD_WAIT_SECS).until(
-            EC.element_to_be_clickable((By.XPATH, self.PLACE_ORDER_XPATH))
+            EC.element_to_be_clickable((By.NAME, self.PLACE_ORDER_BUTTON_NAME))
         ).click()
         return
